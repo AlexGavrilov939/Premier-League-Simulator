5feee0ca32635c61b94d577f031299055df02a1b
Alex Gavrilov <alexgavrilov939@gmail.com>
2022-04-03 22:56:26 +0300

    some test fixes

--- config/app.php
+++ config/app.php
@@ -177,7 +177,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\Passwords\CPPasswordResetServiceProvider::class,
-        App\Providers\HorizonServiceProvider::class,
     ],
 
     /*
